3f7f75b76de6c26309ecdd02f0dd9239034cd383
Leonel Fernandes <leonel.p.d.fernandes17@gmail.com>
2024-06-16 20:40:35 +0100

    Inicial Tela Inicial terminada mas sem navController.

--- app/src/main/java/pt/ipg/jogodememoria/MainActivity.kt
+++ app/src/main/java/pt/ipg/jogodememoria/MainActivity.kt
@@ -1,16 +1,34 @@
 package pt.ipg.jogodememoria
 
 import android.os.Bundle
+import android.view.Surface
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
 import pt.ipg.jogodememoria.ui.theme.JogoDeMemoriaTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +37,62 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JogoDeMemoriaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                 Surface(
+                     modifier = Modifier.fillMaxSize(),
+                     color = MaterialTheme.colorScheme.background
+                 ) {
+                    TelaInicial(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .wrapContentSize()
                     )
+
                 }
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MemoriaDoJogo(){
+}
+
+@Composable
+fun TelaInicial(modifier: Modifier = Modifier) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+
+    ) {
+        Column {
+            Text(text = "Jogo de Memória",
+                color = Color.Black,
+                fontSize = 35.sp,
+                modifier = Modifier.padding(bottom = 150.dp, start = 50.dp)
+            )
+            Image(painter = painterResource(id = R.drawable.todosdopocoyo), contentDescription = null)
+            Spacer(modifier = Modifier.height(40.dp))
+            Button(onClick = { },
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            ) {
+                Text(text = "Iniciar Jogo")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun JogoDeMemoriaPreview() {
     JogoDeMemoriaTheme {
-        Greeting("Android")
+
+        TelaInicial(
+            modifier = Modifier
+                .fillMaxSize()
+                .wrapContentSize()
+        )
     }
 }
